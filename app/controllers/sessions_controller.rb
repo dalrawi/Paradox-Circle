9fe62a503db8146cb5719f6f050298344ec7d4a2
Joshua Escareno <jescaren@nmsu.edu>
2018-04-06 14:11:48 -0600

    login implementation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,23 @@ class SessionsController < ApplicationController
 	end
   
 	def create
-    user = Google_User.from_omniauth(request.env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to '/home/show'
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			log_in user
+			redirect_to user
+		else
+			flash.now[:danger] = 'Invalid email/password combination' #preliminary error message
+			render 'new'
+		end
+#Commented this out to test login, not ready for push
+ #   user = Google_User.from_omniauth(request.env["omniauth.auth"])
+#    session[:user_id] = user.id
+#    redirect_to '/home/show'
   end
 
   def destroy
-    session[:user_id] = nil
+    #session[:user_id] = nil
+		log_out
     redirect_to root_path
   end
 end
