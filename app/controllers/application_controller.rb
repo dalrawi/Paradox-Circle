287acf20eecb5fdca327316df55d150373af1ee2
dalrawi <dalrawi@nmsu.edu>
2018-04-21 21:52:09 -0600

    removed method causing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user #tell rails we wish to use this in our helpers and views
 	
   #checks to see if the user_id exists within the session
-  def current_user
-    @current_user ||= Google_User.find(session[:user_id]) if session[:user_id]
-  end
+
 
 end
