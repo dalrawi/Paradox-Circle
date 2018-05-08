95ad995167ebcd9b8e7bafe6892d582a813aaf15
dalrawi <dalrawi@nmsu.edu>
2018-05-07 22:19:28 -0600

    small update plus comments

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,5 @@
 class ArtistsController < ApplicationController
+	#method for returning search results
 	def index
 		@search_term = params[:query]
 		@artists = Artist.search(@search_term,"artists")
@@ -8,7 +9,7 @@ class ArtistsController < ApplicationController
 		@artist = Artist.find(params[:id])
 		@tag = @artist.tags.build
 	end
-	#update method for updating tags
+	#update method for updating tags. Not used anymore
   	def artist_params
 		params.require(:tag).permit(:name)
 	end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,14 +1,25 @@
-#responsible for creating a new Tag record
+#responsible for creating a new Tag record and appropriate association to artist
 
 class TagsController < ApplicationController
 	def create
 		@tag = Tag.new(tag_params)
 		@tag.save!
 		#logger.debug "params: #{params.inspect}"
+		#artist id only available to tags controller after submit is clicked.
 		artist = Artist.find_by(id: params[:tag][:artistId])
 		artist.tags << @tag
 		artist.save!
 		redirect_to artist
+		flash[:success]="Tag added"
+	end
+
+	def increaseTagCount
+		#need to update count in Artists_Tags table
+		# UPDATE Artists_Tags
+		# SET count = count + 1
+		# WHERE artist_id=artistId AND tag_id=@tag.id
+		flash[:success]="Tag added"
+		redirect_to 'home#show'
 	end
 
 	def tag_params
@@ -18,5 +29,6 @@ class TagsController < ApplicationController
 	def new
 		@tag = Tag.new(:name)
 	end
+
 
 end
