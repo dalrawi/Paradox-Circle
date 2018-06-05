ffb011572eebaebf1d2fb4f67c5524de33ea5a4a
dalrawi <dalrawi@nmsu.edu>
2018-06-05 15:07:15 -0600

    implemented user history to store last 5 viewed artists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,35 @@
 class ApplicationController < ActionController::Base
 	include SessionsHelper
 	protect_from_forgery
+  before_action :store_history
 	protected
 
 	def current_user
   		@current_user ||= User.find_by(id: session[:user_id])
-  	end
+  end
   	
-  	helper_method :current_user, :signed_in?
+  helper_method :current_user, :signed_in?
 
-  	def current_user=(user)
-    	@current_user = user
-    	log_out
-    	session[:user_id] = user.id
-  	end
-  	
-  	private
-  	
-  	#Confirms a logged in user
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-       end
-     end
+  def current_user=(user)
+    @current_user = user
+    log_out
+    session[:user_id] = user.id
+  end
+
+  #Confirms a logged in user
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  
+  #stores the last 5 visited artist pages
+  def store_history
+    session[:history] ||= []
+    session[:history].delete_at(0) if session[:history].size >= 5
+    #only store urls for artist pages and no duplicates
+    session[:history] << request.url if request.url.include? "artist" and session[:history].exclude?(request.url)
+  end
 end
